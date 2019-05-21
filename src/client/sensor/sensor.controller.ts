ae931070ee97f30b72f4126b4d6e4786ed9645d5
Frank Merema <frank.merema@asml.com>
2019-05-21 12:47:12 +0200

    Added sensor service

--- src/client/sensor/sensor.controller.ts
+++ src/client/sensor/sensor.controller.ts
@@ -1,9 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { SensorModel, State } from '@shared/models';
 import { SensorService } from '@shared/service';
+import { Observable } from 'rxjs';
 
 @Controller('sensor')
 export class SensorController {
 
     constructor(private sensorService: SensorService) {
+    }
+
+    @Get('/state/:pin')
+    getStateOfSensor(@Param('pin') pin: number): Observable<State> {
+        return this.sensorService.getStateOfSensor(pin);
+    }
+
+    @Post()
+    addSensor(@Body() pin: number): Observable<SensorModel> {
+        return this.sensorService.addSensor(pin);
+    }
+
+    @Delete('/:pin')
+    deleteSensor(@Param('pin') pin: number): Observable<null> {
+        return this.sensorService.deleteSensor(pin);
     }
 }
--- src/shared/services/sensor/sensor.service.ts
+++ src/shared/services/sensor/sensor.service.ts
@@ -1,8 +1,78 @@
-import { HttpService, Injectable } from '@nestjs/common';
+import { ConfigService } from '@config';
+import { BadRequestException, HttpService, Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { createGpio, errorHandler, readPinState, watchPinState } from '@shared/helpers';
+import { SensorModel, State } from '@shared/models';
+import { HostService } from '@shared/service';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class SensorService {
+export class SensorService implements OnApplicationBootstrap {
 
-    constructor(private httpService: HttpService) {
+    private sensorList: { [key: number]: any } = {};
+
+    constructor(private httpService: HttpService,
+                private configService: ConfigService,
+                private hostService: HostService) {
+    }
+
+    onApplicationBootstrap(): void {
+        this.determineInitialState();
+    }
+
+    getStateOfSensor(pin: number): Observable<number> {
+        if (this.sensorList[pin]) {
+            return readPinState(this.sensorList[pin]);
+        } else {
+            throw new BadRequestException(`No sensor for pin: ${pin}`);
+        }
+    }
+
+    addSensor(pin: number): Observable<SensorModel> {
+        if (!pin) {
+            throw new BadRequestException(`Should set the output pin!`);
+        }
+
+        if (this.sensorList[pin]) {
+            throw new BadRequestException(`Already a sensor registered to this pin: ${pin}!`);
+        }
+
+        return createGpio(pin, 'in', 'both')
+            .pipe(map((gpio: any) => {
+                this.sensorList[pin] = gpio;
+                watchPinState(gpio, pin, (newValue, pin) => this.stateChanged(newValue, pin));
+
+                return {pin: pin, direction: 'in', state: State.OFF};
+            }));
+    }
+
+    deleteSensor(pin: number): Observable<null> {
+        delete this.sensorList[pin];
+
+        return of(null);
+    }
+
+    private determineInitialState(): void {
+        this.httpService.get(`${this.configService.get('homeServerHost')}:${this.configService.get('homeServerPort')}/api/server/sensor/all/${this.hostService.currentHost.id}`)
+            .subscribe(res => {
+                res.data.foreach((s: SensorModel) => {
+                    createGpio(s.pin, 'in', 'both')
+                        .subscribe(gpio => {
+                            this.sensorList[s.pin] = gpio;
+
+                            watchPinState(gpio, s.pin, (newValue, pin) => this.stateChanged(newValue, pin));
+                        });
+                });
+            }, error => {
+                errorHandler(error);
+                setTimeout(() => this.determineInitialState(), 2000);
+            });
+    }
+
+    private stateChanged(newValue: number, pin: number): void {
+        this.httpService.put(`${this.configService.get('homeServerHost')}:${this.configService.get('homeServerPort')}/api/server/sensor/${this.hostService.currentHost.id}/${pin}`, {state: newValue})
+            .subscribe(() => {
+                },
+                errorHandler);
     }
 }
